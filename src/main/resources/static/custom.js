4d3667ea11737c9cec3531bb93ec0640a76cdeb7
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-11-09 11:10:52 -0500

    Add additional map control functions

--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -1,7 +1,28 @@
 var map;
+var marker;
+var coords = { lat: 39.9612, lng: -82.9988 };
+var contentString = '<h2>Columbus, OH</h2> <p>Where your dreams come true.</p>';
+
 function initMap() {
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+    var image = {
+        url: '/diamonds.png',
+        scaledSize: new google.maps.Size(50, 50)
+    };
     map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 39.9612, lng: -82.9988 },
-        zoom: 10
+        center: coords,
+        zoom: 10,
+        scrollwheel: false
+    });
+    marker = new google.maps.Marker({
+        position: coords,
+        map: map,
+        icon: image,
+        animation: google.maps.Animation.DROP
+    });
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.open(map, marker);
     });
 }
